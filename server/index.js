599fd868a8e385556c884987083bae3e6a00d01c
Shtyfliuk Olha <shtyfliuko@gmail.com>
2025-11-20 16:47:39 +0200

    added logic for authentication and authorization in node

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authroutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
+import { verify } from 'crypto';
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +36,12 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage });
+
+/* ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
+
+/* ROUTES */
+app.use('/auth', authroutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
