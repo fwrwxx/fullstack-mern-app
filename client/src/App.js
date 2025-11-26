f661cc6463d744a25d7f5a40a4648acd3c051132
Shtyfliuk Olha <shtyfliuko@gmail.com>
2025-11-26 14:55:18 +0200

    created react redux architecture and added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { use } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
