b17a691d6d14a3b42166ec2e159bdeb1bbd5cd21
Shtyfliuk Olha <shtyfliuko@gmail.com>
2025-11-27 18:33:14 +0200

    added redirects to login page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { themeSettings } from 'theme';
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
@@ -19,9 +20,9 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Routes>
-            <Route path="/home" element={<HomePage />} />
             <Route path="/" element={<LoginPage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+            <Route path="/profile/:userId" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
           </Routes>
           </ThemeProvider>
       </BrowserRouter>
